14aa367586265a29d550e89ef2a8415181c0a9b2
Prashant43226 <53564724+Prashant43226@users.noreply.github.com>
2021-03-14 16:42:47 +0530

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,3 @@
 module.exports={
     url:""//Type mongodb url here if on mongodb atlas
-    //for me it was mongodb+srv://Prashant43226:Prashant@98@cluster0.l2xpy.mongodb.net/Xmeme?retryWrites=true&w=majority
 }
